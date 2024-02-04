66932d54ec945e522cabf829f69e8d3e338cae1a
om-exe <ajinkya9876543@gmail.com>
2024-02-04 19:40:39 +0530

    Code

--- C++/tut9.cpp
+++ C++/tut9.cpp
@@ -1,19 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    // int i;
-    // cout<<"Enter your number : ";
-    // cin>>a;
+    // Break
    for (int i = 0; i < 100; i++)
    {
      if (i==12){
         break;          // When the expression is matched it elimate the loop..
      }
-    cout<<i<<endl;
-     
+    cout<<i<<endl;     
    }
 
-
+    // Continue
    for (int a = 0; a < 100; a++)
    {
      if (a==12){
